9d5b1373a44edd265e652d536cf9fb988eb07b6f
alen.badrajan <https://alen.badrajan@titanium-soft.com>
2018-10-24 15:57:16 +0300

    changes in vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -6,6 +6,10 @@ variable "PATH_TO_PUBLIC_KEY" {
 }
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
+variable "INSTANCE_DEVICE_NAME" {
+  default = "test_jenkins"
+}
+
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
