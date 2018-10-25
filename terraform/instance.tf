89c577acd6770a9eaf34de7a4c9d3955935267a8
alen.badrajan <https://alen.badrajan@titanium-soft.com>
2018-10-25 13:57:24 +0300

    some changes

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
   
   # the public SSH key
-  key_name = "${aws_key_pair.mykeypair-hyperledger.key_name}"
+  key_name = "${aws_key_pair.mykey-tit.key_name}"
 
     # provisioner "remote-exec" {
     # inline = [
--- terraform/key.tf
+++ terraform/key.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "mykeypair-hyperledger" {
-  key_name = "mykeypair-hyperledger"
+resource "aws_key_pair" "mykey-tit" {
+  key_name = "mykey-tit"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
 }
